3b48c85e076a1e41415401120e6eec976859829f
Evandro Roberto Ropelatto FIlho <evandroropfilho@gmail.com>
2024-03-18 11:45:45 -0300

    Alterações

---
+++ BackEnd/Middlewares/AuthRegister.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt');
+const User = require('./Models/Database/User');
+
+exports.registerUser = async (req, res) => {
+  const { nome, email, password, confirmpassword } = req.body;
+
+  // Validação de dados obrigatórios
+  if (!nome || !email || !password || !confirmpassword) {
+    return res
+      .status(400)
+      .json({ message: 'Todos os campos são obrigatórios!' });
+  }
+
+  // Hash da senha
+  const hash = bcrypt.hashSync(password, 10);
+
+  // Verificar se o email já está sendo usado
+  const emailExistente = await User.findOne({ email: email });
+
+  if (emailExistente) {
+    return res.status(400).json({ message: 'Email já cadastrado!' });
+  }
+
+  // Criação do novo usuário
+  const usuario = new User({
+    nome,
+    email,
+    password: hash,
+    confirmpassword: hash,
+  });
+
+  // Verficação de Erros
+  try {
+    await usuario.save();
+    console.log('Usuário criado com sucesso!');
+    return res.status(201).json({ message: 'Usuário criado com sucesso!' });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: 'Erro ao criar usuário!' });
+  }
+
+  // Inserção do usuário no banco de dados
+
+  usuario.save((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Erro ao criar usuário!" });
+    }
+
+    console.log("Usuário criado com sucesso!");
+    return res.status(201).json({ message: "Usuário criado com sucesso!" });
+  });
+};
--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,7 +7,8 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const path = require("path");
 const app = express();
-const authMiddleware = require('./Models/Middlewares/Auth');
+const authMiddleware = require('./Models/Middlewares/CheckToken');
+const authRegister = require('./Models/Middlewares/AuthRegister');
 
 // Config Express
 
@@ -44,61 +45,8 @@ app.get("/user/:id", authMiddleware.checkToken, async (req, res) => {
 });
 // Registro de Usuário
 
-app.post("/registro", async (req, res) => {
-  const { nome, email, password, confirmpassword } = req.body;
+app.post('/registro', authRegister.registerUser);
 
-  // Validação de dados obrigatórios
-
-  if (!nome || !email || !password || !confirmpassword) {
-    return res
-      .status(400)
-      .json({ message: "Todos os campos são obrigatórios!" });
-  }
-
-  // Hash da senha
-
-  const hash = bcrypt.hashSync(password, 10);
-
-  // Verificar se o email já está sendo usado
-
-  const emailExistente = await User.findOne({ email: email });
-
-  if (emailExistente) {
-    return res.status(400).json({ message: "Email já cadastrado!" });
-  }
-
-  // Criação do novo usuário
-
-  const usuario = new User({
-    nome,
-    email,
-    password: hash,
-    confirmpassword: hash,
-  });
-
-  // Verficação de Erros
-
-  try {
-    await usuario.save();
-    console.log("Usuário criado com sucesso!");
-    return res.status(201).json({ message: "Usuário criado com sucesso!" });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "Erro ao criar usuário!" });
-  }
-
-  // Inserção do usuário no banco de dados
-
-  usuario.save((err) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ message: "Erro ao criar usuário!" });
-    }
-
-    console.log("Usuário criado com sucesso!");
-    return res.status(201).json({ message: "Usuário criado com sucesso!" });
-  });
-});
 
 // Login do Usuário
 app.get("/login", (req, res) => {
