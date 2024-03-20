7b971aedb78e34815c6e50fe51309d48f231b1fe
Evandro Roberto Ropelatto FIlho <evandroropfilho@gmail.com>
2024-03-20 10:49:37 -0300

    Alterações função gettodos

--- BackEnd/FrontEnd/todo.js
+++ BackEnd/FrontEnd/todo.js
@@ -112,40 +112,44 @@ async function Remove(todoText) {
   }
 }
 
-function getTodos() {
-  //Check: if item/s are there;
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
+async function getTodos() {
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const db = client.db("test");
+  const collection = db.collection("tarefas");
+
+  try {
+    const todos = await collection.find().toArray();
+
+    todos.forEach(function (todo) {
+      // toDo DIV;
+      const toDoDiv = document.createElement("div");
+      toDoDiv.classList.add("todo", `${savedTheme}-todo`);
+
+      // Create LI
+      const newToDo = document.createElement("li");
+
+      newToDo.innerText = todo.text;
+      newToDo.classList.add("todo-item");
+      toDoDiv.appendChild(newToDo);
+
+      // check btn;
+      const checked = document.createElement("button");
+      checked.innerHTML = '<i class="fas fa-check"></i>';
+      checked.classList.add("check-btn", `${savedTheme}-button`);
+      toDoDiv.appendChild(checked);
+      // delete btn;
+      const deleted = document.createElement("button");
+      deleted.innerHTML = '<i class="fas fa-trash"></i>';
+      deleted.classList.add("delete-btn", `${savedTheme}-button`);
+      toDoDiv.appendChild(deleted);
+
+      // Append to list;
+      toDoList.appendChild(toDoDiv);
+    });
+  } catch (err) {
+    console.error('Erro ao obter os todos:', err);
+  } finally {
+    client.close();
   }
-
-  todos.forEach(function (todo) {
-    // toDo DIV;
-    const toDoDiv = document.createElement("div");
-    toDoDiv.classList.add("todo", `${savedTheme}-todo`);
-
-    // Create LI
-    const newToDo = document.createElement("li");
-
-    newToDo.innerText = todo;
-    newToDo.classList.add("todo-item");
-    toDoDiv.appendChild(newToDo);
-
-    // check btn;
-    const checked = document.createElement("button");
-    checked.innerHTML = '<i class="fas fa-check"></i>';
-    checked.classList.add("check-btn", `${savedTheme}-button`);
-    toDoDiv.appendChild(checked);
-    // delete btn;
-    const deleted = document.createElement("button");
-    deleted.innerHTML = '<i class="fas fa-trash"></i>';
-    deleted.classList.add("delete-btn", `${savedTheme}-button`);
-    toDoDiv.appendChild(deleted);
-
-    // Append to list;
-    toDoList.appendChild(toDoDiv);
-  });
 }
 
